a64e748dcbbb9690cd19ee86f82f13d44b243f8c
Bryan Irace <bryan.irace@gmail.com>
2012-08-28 00:17:39 -0400

    Add OAuth support

--- Tests/TMTumblrSDKTests.m
+++ Tests/TMTumblrSDKTests.m
@@ -10,22 +10,95 @@
 
 #import "TMAPIClient.h"
 
+@interface TMTumblrSDKTests()
+
+@property (nonatomic, copy) TMAPISuccessCallback defaultSuccessCallback;
+@property (nonatomic, copy) TMAPIErrorCallback defaultErrorCallback;
+@property (nonatomic, assign) BOOL receivedAsynchronousCallback;
+
+- (void)performAsynchronousTest:(void(^)(void))testCode;
+
+@end
+
 @implementation TMTumblrSDKTests
+
+#pragma mark - Tests
+
+//- (void)testBlogInfo {
+//    [self performAsynchronousTest:^ {
+//        [[TMAPIClient sharedInstance] blogInfo:@"bryan" success:self.defaultSuccessCallback
+//                                         error:self.defaultErrorCallback];
+//    }];
+//}
+
+- (void)testFollowers {
+    [self performAsynchronousTest:^ {
+        [[TMAPIClient sharedInstance] followers:@"bryan" limit:20 offset:0
+                                        success:self.defaultSuccessCallback
+                                          error:self.defaultErrorCallback];
+    }];
+}
+
+#pragma mark - Common
 
 - (void)setUp {
     [super setUp];
-}
-
-- (void)tearDown {
-    [super tearDown];
-}
-
-- (void)testBlogInfo {
-    [[TMAPIClient sharedInstance] blogInfo:@"bryan" success:^(NSDictionary *result) {
-        NSLog(@"Result: %@", result);
+    
+    static dispatch_once_t predicate;
+    
+    dispatch_once(&predicate, ^{
+        NSDictionary *credentials = [[NSDictionary alloc] initWithContentsOfFile:
+                                     [[NSBundle bundleForClass:[TMTumblrSDKTests class]] pathForResource:@"Credentials"
+                                                                                                  ofType:@"plist"]];
         
-        STAssertNotNil(result, nil);
-    } error:nil];
+        NSString *APIKey = credentials[@"APIKey"];
+        NSString *OAuthConsumerKey = credentials[@"OAuthConsumerKey"];
+        NSString *OAuthConsumerSecret = credentials[@"OAuthConsumerSecret"];
+        NSString *OAuthToken = credentials[@"OAuthToken"];
+        NSString *OAuthTokenSecret = credentials[@"OAuthTokenSecret"];
+        
+        STAssertTrue(APIKey.length, @"APIKey required in Credentials.plist");
+        STAssertTrue(OAuthConsumerKey.length, @"OAuthConsumerKey required in Credentials.plist");
+        STAssertTrue(OAuthConsumerSecret.length, @"OAuthConsumerSecret required in Credentials.plist");
+        STAssertTrue(OAuthToken.length, @"OAuthToken required in Credentials.plist");
+        STAssertTrue(OAuthTokenSecret.length, @"OAuthTokenSecret required in Credentials.plist");
+        
+        [TMAPIClient sharedInstance].APIKey = APIKey;
+        [TMAPIClient sharedInstance].OAuthConsumerKey = OAuthConsumerKey;
+        [TMAPIClient sharedInstance].OAuthConsumerSecret = OAuthConsumerSecret;
+        [TMAPIClient sharedInstance].OAuthToken = OAuthToken;
+        [TMAPIClient sharedInstance].OAuthTokenSecret = OAuthTokenSecret;
+        
+        [credentials release];
+        
+        self.defaultSuccessCallback = ^ (NSDictionary *result) {
+            NSLog(@"Result: %@", result);
+            
+            STAssertNotNil(result, nil);
+            
+            self.receivedAsynchronousCallback = YES;
+        };
+        
+        self.defaultErrorCallback = ^ (NSError *error, NSArray *validationErrors) {
+            STFail(@"Error callback received");
+            
+            self.receivedAsynchronousCallback = YES;
+        };
+    });
+}
+
+- (void)performAsynchronousTest:(void(^)(void))testCode {    
+    self.receivedAsynchronousCallback = NO;
+    
+    testCode();
+    
+    NSDate *loopUntil = [NSDate dateWithTimeIntervalSinceNow:10];
+    
+    while (!self.receivedAsynchronousCallback && [loopUntil timeIntervalSinceNow] > 0)
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:loopUntil];
+    
+    if (!self.receivedAsynchronousCallback)
+        STFail(@"Request timed out");
 }
 
 @end
---
+++ TumblrSDK/TMOAuth.m
@@ -0,0 +1,153 @@
+//
+//  TMOAuth.m
+//  TumblrSDK
+//
+//  Created by Bryan Irace on 8/27/12.
+//  Copyright (c) 2012 Bryan Irace. All rights reserved.
+//
+
+#import "TMOAuth.h"
+
+#import <CommonCrypto/CommonDigest.h>
+#import <CommonCrypto/CommonHMAC.h>
+#import <sys/sysctl.h>
+#import "NSData+Base64.h"
+
+@implementation TMOAuth
+
+// TODO: Ensure this works without OAuthToken, OAuthTokenSecret
+
++ (NSString *)authorizationHeaderForRequest:(JXHTTPOperation *)request nonce:(NSString *)nonce
+                                consumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret
+                                      token:(NSString *)token tokenSecret:(NSString *)tokenSecret {
+    NSMutableDictionary *headerParameters = [[@{
+        @"oauth_timestamp" : UNIXTimestamp([NSDate date]),
+        @"oauth_nonce" : nonce,
+        @"oauth_version" : @"1.0",
+        @"oauth_signature_method" : @"HMAC-SHA1",
+        @"oauth_consumer_key" : consumerKey,
+        @"oauth_token" : token
+    } mutableCopy] autorelease];
+    
+    NSMutableDictionary *signatureParameters = [NSMutableDictionary dictionaryWithDictionary:headerParameters];
+
+    NSString *queryString = request.requestURL.query;
+    
+    if (queryString) {
+        [signatureParameters addEntriesFromDictionary:queryStringToDictionary(queryString)];
+    }
+    
+    // Assuming body format application/x-www-form-urlencoded
+    NSDictionary *postBodyParameters = ((JXHTTPFormEncodedBody *)request.requestBody).dictionary;
+    
+    for (NSString *key in postBodyParameters) {
+        signatureParameters[key] = URLEncode(postBodyParameters[key]);
+    }
+    
+    NSMutableArray *parameters = [NSMutableArray array];
+    
+    void (^addParameter)(NSString *key, id value) = ^(NSString *key, id value) {
+        [parameters addObject:[NSString stringWithFormat:@"%@=%@", key, URLEncode(value)]];
+    };
+    
+    for (NSString *key in [[signatureParameters allKeys] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)]) {
+        id value = signatureParameters[key];
+        
+        // TODO: Can use Underscore-like function here?
+        
+        if ([value isKindOfClass:[NSArray class]]) {
+            for (id arrayValue in (NSArray *)value) {
+                addParameter(key, arrayValue);
+            }
+        } else {
+            addParameter(key, value);
+        }
+    }
+    
+    NSString *parameterString = [parameters componentsJoinedByString:@"&"];
+    
+    NSURL *URL = request.requestURL;
+    
+    NSURL *baseURL = [[NSURL alloc] initWithScheme:URL.scheme host:URL.host path:URL.path];
+    
+    NSMutableString *rawSignature = [NSString stringWithFormat:@"%@&%@&%@", request.requestMethod,
+                                     URLEncode([baseURL absoluteString]),
+                                     URLEncode(parameterString)];
+    
+    // Hash the raw signature string into an encrypted signature
+    NSString *keyString = [NSString stringWithFormat:@"%@&%@", consumerSecret, tokenSecret];
+    
+    // Add the encrypted signature to the header parameter dictionary
+    headerParameters[@"oauth_signature"] = [HMACSHA1(rawSignature, keyString) base64EncodedString];
+    
+    NSMutableArray *components = [NSMutableArray array];
+    
+    for (NSString *key in headerParameters) {
+        [components addObject:[NSString stringWithFormat:@"%@=\"%@\"", key, URLEncode(headerParameters[key])]];
+    }
+    
+    return [NSString stringWithFormat:@"OAuth %@", [components componentsJoinedByString:@","]];
+}
+
+#pragma mark - Convenience functions
+
+static inline NSString *URLDecode(NSString *string) {
+    return [(NSString *)CFURLCreateStringByReplacingPercentEscapes(NULL, (CFStringRef)string, CFSTR("")) autorelease];
+}
+
+static inline NSString *URLEncode(NSString *string) {
+    return [(NSString *)CFURLCreateStringByAddingPercentEscapes(NULL, (CFStringRef)string, NULL, CFSTR("!*'();:@&=+$,/?%#[]%"), kCFStringEncodingUTF8) autorelease];
+}
+
+static inline NSString *UNIXTimestamp(NSDate *date) {
+    return [NSString stringWithFormat:@"%d", (int)[date timeIntervalSince1970]];
+}
+
+static inline NSDictionary *queryStringToDictionary(NSString *query) {
+    NSMutableDictionary *mutableParameterDictionary = [[[NSMutableDictionary alloc] init] autorelease];
+    
+    NSArray *parameters = [query componentsSeparatedByString:@"&"];
+    
+    for (NSString *parameter in parameters) {
+        NSArray *keyValuePair = [parameter componentsSeparatedByString:@"="];
+        
+        if (keyValuePair.count != 2) {
+            // TODO: Possible for a parameter to have only a key and no value
+            continue;
+        }
+        
+        NSString *parameterKey = URLDecode([keyValuePair objectAtIndex:0]);
+        NSString *parameterValue = URLDecode([keyValuePair objectAtIndex:1]);
+        
+        [mutableParameterDictionary setObject:parameterValue forKey:parameterKey];
+    }
+    
+    return [NSDictionary dictionaryWithDictionary:mutableParameterDictionary];
+}
+
+static inline NSData *HMACSHA1(NSString *dataString, NSString *keyString) {
+    NSData *data = [dataString dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *key = [keyString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    void *buffer = malloc(CC_SHA1_DIGEST_LENGTH);
+    CCHmac(kCCHmacAlgSHA1, [key bytes], [key length], [data bytes], [data length], buffer);
+    
+    return [NSData dataWithBytesNoCopy:buffer length:CC_SHA1_DIGEST_LENGTH freeWhenDone:YES];
+}
+
+static inline NSString *stringParameterValueForObject(NSObject *object) {
+    if ([object isKindOfClass:[NSString class]]) {
+        return (NSString *)object;
+        
+    } else if ([object isKindOfClass:[NSNumber class]]) {
+        return [(NSNumber *)object stringValue];
+        
+    } else if ([object isKindOfClass:[NSDate class]]) {
+        return UNIXTimestamp((NSDate *)object);
+    }
+    
+    return nil;
+}
+
+@end
+
