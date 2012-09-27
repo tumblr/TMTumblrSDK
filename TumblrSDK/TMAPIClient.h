e126bcabe0dec76eff464d56dd9e8ae8908b9c45
Bryan Irace <bryan.irace@gmail.com>
2012-09-27 18:22:24 -0400

    Combining back into one file

--- TumblrSDK/TMAPIClient.h
+++ TumblrSDK/TMAPIClient.h
@@ -7,6 +7,7 @@
 //
 
 typedef void (^TMAPICallback)(id, NSError *error);
+typedef void (^TMAPIDataCallback)(NSData *, NSError *error);
 
 // TODO: Evaluate the usefulness of these
 extern NSString * const TMAPIParameterLimit;
@@ -32,8 +33,72 @@ extern NSString * const TMAPIParameterType;
 
 - (JXHTTPOperation *)post:(NSString *)path parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
 
-- (void)sendRequest:(JXHTTPOperation *)request;
-
 - (void)sendRequest:(JXHTTPOperation *)request callback:(TMAPICallback)callback;
+
+// Authentication
+
+- (JXHTTPOperation *)xAuthRequest:(NSString *)userName password:(NSString *)password callback:(TMAPICallback)callback;
+
+// User
+
+- (JXHTTPOperation *)userInfo:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)dashboard:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)likes:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)following:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)follow:(NSString *)blogName callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)unfollow:(NSString *)blogName callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)like:(NSString *)postID reblogKey:(NSString *)reblogKey callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)unlike:(NSString *)postID reblogKey:(NSString *)reblogKey callback:(TMAPICallback)callback;
+
+// Blog
+
+- (JXHTTPOperation *)blogInfo:(NSString *)blogName callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)followers:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)avatar:(NSString *)blogName size:(int)size callback:(TMAPIDataCallback)callback;
+
+- (JXHTTPOperation *)posts:(NSString *)blogName type:(NSString *)type parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)queue:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)drafts:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)submissions:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+// Posting
+
+- (JXHTTPOperation *)editPost:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)reblogPost:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)createPost:(NSString *)blogName type:(NSString *)type parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)deletePost:(NSString *)blogName id:(NSString *)postID callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)text:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)quote:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)link:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)chat:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)audio:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)video:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+- (JXHTTPOperation *)photo:(NSString *)blogName parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
+
+// Tagging
+
+- (JXHTTPOperation *)tagged:(NSString *)tag parameters:(NSDictionary *)parameters callback:(TMAPICallback)callback;
 
 @end
--- TumblrSDK/TMTumblrSDK.h
+++ TumblrSDK/TMTumblrSDK.h
@@ -9,8 +9,3 @@
 #import "JXHTTP.h"
 
 #import "TMAPIClient.h"
-#import "TMAPIClient+Auth.h"
-#import "TMAPIClient+Blog.h"
-#import "TMAPIClient+Posting.h"
-#import "TMAPIClient+User.h"
-#import "TMAPIClient+Tagging.h"
