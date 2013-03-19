6f0ec829266ad626cef6ee4b59e4728f7ae86642
Bryan Irace <bryan.irace@gmail.com>
2013-03-19 09:59:34 -0400

    Add UIActivity

---
+++ TumblrSDK/TMTumlbrActivity.h
@@ -0,0 +1,21 @@
+//
+//  TMTumlbrActivity.h
+//  TumblrSDK
+//
+//  Created by Bryan Irace on 3/19/13.
+//  Copyright (c) 2013 Tumblr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ `TMTumblrActivity` provides the bare minimum `UIActivity` subclass necessary to start sharing to Tumblr: just the name
+ and icon. 
+ 
+ For now this is essentially a blank canvas to be used in conjunction with either `TMAPIClient` or `TMTumblrAppClient`.
+ This should be subclassed in accordance with the `UIActivity` documentation to actually perform an action based on
+ provided activity items.
+ */
+@interface TMTumlbrActivity : UIActivity
+
+@end
