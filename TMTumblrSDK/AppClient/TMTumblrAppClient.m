674ec63b2df7f846421f34ae94d1fdf5c6183fa6
Bryan Irace <bryan.irace@gmail.com>
2013-03-20 11:05:31 -0400

    Remove methods that aren't there

--- TMTumblrSDK/AppClient/TMTumblrAppClient.m
+++ TMTumblrSDK/AppClient/TMTumblrAppClient.m
@@ -36,20 +36,4 @@
     [self performAction:@"blog" parameters:@{ @"blogName" : blogName, @"postID" : postID }];
 }
 
-- (void)createTextPost:(NSString *)title body:(NSString *)body {
-    [self performAction:@"text" parameters:@{ @"title" : title, @"body" : body }];
-}
-
-- (void)createQuotePost:(NSString *)quote source:(NSString *)source {
-    [self performAction:@"quote" parameters:@{ @"quote" : quote, @"source" : source }];
-}
-
-- (void)createLinkPost:(NSString *)title URL:(NSString *)URL description:(NSString *)description {
-    [self performAction:@"link" parameters:@{ @"title" : title, @"url" : URL, @"description" : description }];
-}
-
-- (void)createChatPost:(NSString *)title body:(NSString *)body {
-    [self performAction:@"chat" parameters:@{ @"title" : title, @"body" : body }];
-}
-
 @end
