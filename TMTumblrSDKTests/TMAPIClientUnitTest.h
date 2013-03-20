45075a685d999cad22b9b71c54113b1f476af68a
John Crepezzi <john.crepezzi@gmail.com>
2013-03-20 17:01:29 -0400

    Update code style

--- TMTumblrSDKTests/TMAPIClientUnitTest.h
+++ TMTumblrSDKTests/TMAPIClientUnitTest.h
@@ -16,14 +16,29 @@
 
 @property(nonatomic, retain) TMAPIClient *client;
 
+// Set up the Test with a TMAPIClient
 - (void) setUp;
 
+// Assert that with a given callback being fired, we can verify the response is
+// of a certain form
 - (void) assertCallback:(void(^)(id, TMAPICallback))action andVerify:(JXHTTPOperation*)op;
+
+// Verify that two requests are the same
 - (void) assertSimilarRequest:(JXHTTPOperation*)op1 to:(JXHTTPOperation*)op2;
+
+// Verify that the multipart body of a request is a certain NSString
 - (void) assertMultipartBody:(JXHTTPOperation*)op is:(NSString*)expected;
+
+// Verify that the body of a request is a certain NSString
 - (void) assertBody:(JXHTTPOperation*)op is:(NSDictionary*)expected;
+
+// Verify that the query of a request is a certain NSString
 - (void) assertQuery:(JXHTTPOperation*)op is:(NSString*)expected;
--(void) assertPath:(JXHTTPOperation*)op is:(NSString*)expected;
--(void) assertMethod:(JXHTTPOperation*)op is:(NSString*)expected;
+
+// Verify that the part of a request is a certain NSString
+- (void) assertPath:(JXHTTPOperation*)op is:(NSString*)expected;
+
+// Verify that the request method of a request is a certain NSString
+- (void) assertMethod:(JXHTTPOperation*)op is:(NSString*)expected;
 
 @end
