2a6041a303716898d6ec09b5be01206f7c65aef2
John Crepezzi <john.crepezzi@gmail.com>
2013-03-20 14:19:15 -0400

    Split up tests into separate files for readability

---
+++ TMTumblrSDKTests/TMAPIClientBlogTest.h
@@ -0,0 +1,14 @@
+//
+//  TMAPIClientBlogTest.h
+//  TMTumblrSDK
+//
+//  Created by John Crepezzi on 3/20/13.
+//  Copyright (c) 2013 Tumblr. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "TMAPIClientUnitTest.h"
+
+@interface TMAPIClientBlogTest : TMAPIClientUnitTest
+
+@end
