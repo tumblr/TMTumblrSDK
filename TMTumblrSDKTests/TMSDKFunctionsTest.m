acbc17ba1489d9b07065b6a941bc510512141645
Bryan Irace <bryan.irace@gmail.com>
2013-04-01 00:08:19 -0400

    Unit tests

--- TMTumblrSDKTests/TMSDKFunctionsTest.m
+++ TMTumblrSDKTests/TMSDKFunctionsTest.m
@@ -12,12 +12,39 @@
 
 @implementation TMSDKFunctionsTest
 
-- (void)testQueryToDictionary {
-    NSDictionary *dictionary = TMQueryStringToDictionary(@"title=SomeTitle&body=SomeBody");
-    dictionary = TMQueryStringToDictionary(@"title=SomeTitle&tag=lol&tag=gif");
+- (void)testQueryToDictionaryWithTwoParameters {
+    NSString *title = @"Some title";
+    NSString *body = @"LOL";
+    
+    NSDictionary *dictionary = TMQueryStringToDictionary([NSString stringWithFormat:@"title=%@&body=%@",
+                                                          TMURLEncode(title), TMURLEncode(body)]);
+    
+    STAssertTrue([[dictionary allKeys] count] == 2, @"Incorrect number of dictionary keys");
+    STAssertEqualObjects(dictionary[@"title"], title, @"Title parameter is incorrect");
+    STAssertEqualObjects(dictionary[@"body"], body, @"Body parameter is incorrect");
 }
 
-- (void)testDictionaryToQuery {
+- (void)testQueryToDictionaryWithRepeatedParameter {
+    NSString *title = @"Title";
+    NSString *tag1 = @"l  o  l";
+    NSString *tag2 = @"gi://$@DAISDJ++++DSADf";
+    NSString *tag3 = @"foo=bar&baz+foo2";
+
+    NSDictionary *dictionary = TMQueryStringToDictionary([NSString stringWithFormat:@"title=%@&tag=%@&tag=%@&tag=%@",
+                                                          TMURLEncode(title), TMURLEncode(tag1), TMURLEncode(tag2), TMURLEncode(tag3)]);
+
+    NSArray *tags = @[tag1, tag2, tag3];
+    
+    STAssertTrue([[dictionary allKeys] count] == 2, @"Incorrect number of dictionary keys");
+    STAssertEqualObjects(dictionary[@"title"], title, @"Title parameter is incorrect");
+    STAssertEqualObjects(dictionary[@"tag"], tags, @"Tag parameter is incorrect");
+}
+
+- (void)testDictionaryToQueryWithTwoParameters {
+
+}
+
+- (void)testDictionaryToQueryWithRepeatedParameter {
     
 }
 
