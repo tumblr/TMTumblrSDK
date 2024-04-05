ff847280d981458f18e73859d5f5003e7bc6783c
Adriana Elizondo <adriana.elizondo@automattic.com>
2024-04-05 10:37:44 -0600

    updates package file to only export TMTumblrSDK target

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "TMTumblrSDK",
-            targets: ["TMTumblrSDK", "TMTumblrSDKTests", "TMTumblrSDKSwiftTests"]),
+            targets: ["TMTumblrSDK"]),
     ],
     targets: [
         .target(
